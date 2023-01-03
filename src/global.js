4a232f24df57356fd28e512ff89ff47ed8a86849
Hemakrishna-07 <hemky143@.com>
2023-01-03 16:34:58 +0530

    api commit

--- src/global.js
+++ src/global.js
@@ -1,2 +1,3 @@
 // export const API = "https://632161eafd698dfa29f6a0df.mockapi.io"
-export const API = "http://localhost:4000"
+// export const API = "http://localhost:4000"
+export const API = "https://movieapp-23s6.onrender.com"
